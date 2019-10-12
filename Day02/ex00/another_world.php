465e808288c27404cb320757bd28469d0ddb37a3
Tiffany MABUNDA <tmabunda@c5r7s6.wethinkcode.co.za>
2019-10-12 13:16:35 +0200

    OK

--- Day02/ex00/another_world.php
+++ Day02/ex00/another_world.php
@@ -1,20 +1,22 @@
 #!/usr/bin/php
 <?php
-
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    hw.php                                             :+:      :+:    :+:    #
+#    another_world.php                                  :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: tmabunda <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/10/10 09:53:35 by tmabunda          #+#    #+#              #
-#    Updated: 2019/10/10 10:15:40 by tmabunda         ###   ########.fr        #
+#    Created: 2019/10/12 13:10:49 by tmabunda          #+#    #+#              #
+#    Updated: 2019/10/12 13:10:49 by tmabunda         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
-
-
-
-echo "<br />Hello World\n";
-
+function my_cleaner($str)
+  	{
+    	$temp = trim($str);
+    	$res = preg_replace('/(\s+)|(\t+)/', ' ', $temp);
+    	return $res;
+	 }
+	if ($argc > 1)
+		echo my_clean($argv[1])."\n";
 ?>
